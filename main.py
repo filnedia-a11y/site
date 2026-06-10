018f9949267a65d8c03bd08b101d079af35bd27d
filnedia-a11y <filnedia@gmail.com>
2026-06-10 08:39:34 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2103,4 +2103,4 @@ if __name__ == '__main__':
     print(f'🌐 URL: {BASE_URL}')
     print(f'👤 Админ: {ADMIN_USERNAME} / {ADMIN_PASSWORD}')
     print(f'🧮 Математическая капча включена')
-    app.run(host='0.0.0.0', port=PORT, debug=True)
+    app.run(host='0.0.0.0', port=PORT, debug=false)
